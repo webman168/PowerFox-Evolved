0186230912f0040e075c621e1b2c1c7abddf0f94
Jazzzny <jazzzny225@gmail.com>
2026-01-17 02:27:27 -0500

    Branding Pt. 1

--- basilisk/confvars.sh
+++ basilisk/confvars.sh
@@ -3,8 +3,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=Basilisk
-MOZ_APP_VENDOR=Basilisk-Dev
+MOZ_APP_BASENAME=PowerFox
+MOZ_APP_VENDOR=Jazzzny
 MOZ_PHOENIX=1
 MOZ_AUSTRALIS=1
 MC_BASILISK=1
